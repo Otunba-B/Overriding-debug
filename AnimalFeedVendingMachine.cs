016d5aabb78c4157ab8b17804f8cc3f2ade4fe00
Otunba-B <bryanejiofor@ymail.com>
2021-06-16 14:56:47 +0100

    Add project files.

---
+++ AnimalFeedVendingMachine.cs
@@ -0,0 +1,15 @@
+﻿namespace Overriding_debug
+{
+    class AnimalFeedVendingMachine : VendingMachine
+    {
+        public override string Item
+        {
+            get { return " a handul of animal feed. "; }
+        }
+
+        protected override bool CheckAmount(decimal money)
+        {
+            return money >= 1.25M;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Overriding_debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VendingMachine vendingMachine = new AnimalFeedVendingMachine();
+            Console.WriteLine(vendingMachine.Dispense(1.0M));
+         }
+    }
+}
